bbaf190d17b69507c52e3bf827d0c12e3513a9d2
Hanelise Amorim <hanelise.amorim@escola.pr.gov.br>
2023-07-07 10:17:36 -0300

    criação div aula2

--- controller.js
+++ controller.js
@@ -8,3 +8,15 @@ function acessar() {
        window.location.href = "aula2.html";
     }
 }
+
+
+// FUNÇÃO PARA ARMAZENAR NOMES EM ARRAY
+var dadosLista = [];
+
+function salvarUser(){
+    let nomeUser = document.getElementById("nomeUser").value;
+    if(nomeUser){
+        dadosLista.push(nomeUser);
+
+    }
+}
